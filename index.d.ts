e81cf69c43b9400aa12bfaa88c466f7930016e54
Andre Luiz <andreluiz.onete@hotmail.com>
2019-12-31 16:11:54 -0300

    Change logger and set default file to save

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,27 @@
 import logger = require('./lib/omni-loggers');
 
 /**
- * Log on te console te string like > [ 18/12/2019 - 03:53:33 ] - src\index.js:7 -> Teste de Log
- * @param {string} message Message to log on console
- * @param {string} [fileSave] File  to save log
+ * Log on te console te string like > [ 18/12/2019 - 03:53:33 - info ] - src\index.js:7 -> Teste de Log
+ * @param {string|object} message Message to log on console
  * @returns {void}
  */
-declare function parallelLogger(message: string, fileSave?: string): void;
-export = parallelLogger;
+declare function info(message: string | object): void;
+
+/**
+ * Log on te console te string like > [ 18/12/2019 - 03:53:33 - warn ] - src\index.js:7 -> Teste de Log
+ * @param {string|object} message Message to log on console
+ * @returns {void}
+ */
+declare function warn(message: string | object): void;
+
+/**
+ * Log on te console te string like > [ 18/12/2019 - 03:53:33 - error ] - src\index.js:7 -> Teste de Log
+ * @param {string|object} message Message to log on console
+ * @returns {void}
+ */
+declare function error(message: string | object): void;
+export = {
+  info,
+  warn,
+  error
+};
